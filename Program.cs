a9bc6cb5742d337eba5ed1ec02e1d6b8cdfef0e3
nurcin <nurchin.14@gmail.com>
2024-10-09 18:13:50 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace homework
+
+{
+    internal class task1
+    {
+        private static void Main(string[] args)
+        {
+        for (int i = 1; i < 100; i++)
+            {
+                if (i%15==0)
+                {
+
+                    Console.WriteLine(i);
+                }
+        
+            }
+        
+        
+        
+        
+        
+        }
+    }
+}
---
+++ task2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace project
+{
+    internal class task2
+    {
+        static void Main(string[] args)
+        {
+            var grade = 84;
+            if (grade < 65)
+            {
+                Console.WriteLine("Kəsildin");  
+            }
+            else if (grade <85 && grade>=65)
+            {
+                Console.WriteLine("Adi Diplom");
+            }      
+            
+           else  if (grade <95 && grade>=85)
+            {
+                Console.WriteLine("Honour");
+            }
+
+            else
+            {
+                Console.WriteLine("High Honour");
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
